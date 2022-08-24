1f65926397a31d7dd4b6fa2fddb29c61b7b8938b
Dylan Masschelein <dylanmasschelein@gmail.com>
2022-08-24 11:18:33 -0700

    Initialize custom library

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+// import defaultStyles from './button.module.css';
+// import { Icon } from 'semantic-ui-react';
+
+interface ButtonProps {
+	type?: 'button' | 'submit' | 'reset' | undefined;
+	styles?: string | any;
+	handleClick?: any;
+	text: string | JSX.Element;
+	icon?: string | any;
+	iconLoading?: boolean;
+	image?: string;
+	disabled?: boolean | string | any;
+	topMargin?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+	styles,
+	handleClick,
+	text,
+	icon,
+	image,
+	disabled,
+	type = 'button',
+	iconLoading,
+	topMargin
+}) => (
+	<button
+		type={type}
+		onClick={handleClick}
+		// className={`${defaultStyles.base} ${styles} ${disabled && defaultStyles.disabled} ${
+		// 	topMargin && defaultStyles.margin_top
+		// }`}
+		disabled={disabled}
+	>
+		<span style={image ? { marginRight: 12 } : {}}>{`${text} `}</span>
+		{/* {icon && <Icon loading={iconLoading} name={icon} className={defaultStyles.icon} />} */}
+	</button>
+);
+
+export default Button;
